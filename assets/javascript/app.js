42edfed8713725b7f0ce30114c7c3348c301f34c
HEBA ELSAFTY <heba.m.elsafty@gmail.com>
2019-02-12 19:29:07 +0200

    deish game headr

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,7 +45,7 @@ let failedNum = 0;
 
 //Game Moves
 let moves = 0;
-let counter = document.querySelector(".moves-num");
+let counter = document.querySelector("#moves");
 
 
 
@@ -68,9 +68,9 @@ let interval ;
 function satartTimer(){
   interval = setInterval(function () {
     timer.innerHTML = "<div class=\"minutes\"> \
-    <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+    <div class=\"numbers\">" + minutes + "</div>min</div> \
     <div class=\"seconds\"> \
-    <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+    <div class=\"numbers\">" + seconds + "</div>sec</div> \
     </div>";
     ;
     seconds++;
@@ -85,68 +85,74 @@ function satartTimer(){
   }, 500);
 }
 
-function movesCounter(){
-  moves++;
-  moves.innerHTML = moves;
-}
+// function movesCounter(){
+//   moves++;
+//   moves.innerHTML = moves;
+// }
 
-for (let i = 0 ; i < cards.length ; i++){
-  cards[i].addEventListener('click', function (e) { //on click
-    if (!cards[i].classList.contains('open')){ //if box is Unlock
-      // Moves and timer
-      function movesCounter(){
-        moves ++;
-          counter.innerHTML = moves
-        if(moves == 1){
-          secondss = 0;
-          minutes = 0;
-          hours = 0;
-          satartTimer();
-        }
-      }
-      movesCounter();
-
-      cards[i].classList.add('open','card-is-flipped');
-      cards[i].innerHTML = '<div class="front-card"><img src="' + images[i] +'" /></div>';
-      if(openedImage  == null ){ //this is first card
-        openedImage =  images[i];
-        openedCard = cards[i];
-      } else { //second click on another image
-        if(openedImage == images[i] ){ // matched images
-          // score changed
-          //add class mached
-          cards[i].classList.add('matched');
-          openedCard.classList.add('matched');
-
-        } else{
-          let tempFirstCard = openedCard;
-          setTimeout(function(){ //reset cards
-            cards[i].innerHTML = '<div class="back-card"><img src="assets/images/memory-logo.png"></div>'; //reset current box
-            tempFirstCard.innerHTML = '<div class="back-card"><img src="assets/images/memory-logo.png"></div>';
-            tempFirstCard.classList.remove('open','card-is-flipped');
-            cards[i].classList.remove('open','card-is-flipped');
-          },500)
-          var audio = new Audio('assets/audio/NFF-wrong-move.wav');
-          audio.play();
-          failedNum ++ ;
-          if(failedNum == 3){
-            failedNum = 0;
-            if (scoreRateNum > 1 ){
-              scoreRateNum -= 1;
-              drawStars(scoreRateNum);
-            }
+/*
+################
+Start Game
+################
+*/
+function startGame(){
+  for (let i = 0 ; i < cards.length ; i++){
+    cards[i].addEventListener('click', function (e) { //on click
+      if (!cards[i].classList.contains('open')){ //if box is Unlock
+        // Moves and timer
+        function movesCounter(){
+          moves ++;
+            counter.innerHTML = "<span class=\"moves-num\">" + moves + "</span>moves</span>";
+          if(moves == 1){
+            secondss = 0;
+            minutes = 0;
+            hours = 0;
+            satartTimer();
           }
-          //add class notmatched
         }
-        openedImage = null;
+        movesCounter();
+
+        cards[i].classList.add('open','card-is-flipped');
+        cards[i].innerHTML = '<div class="front-card"><img src="' + images[i] +'" /></div>';
+        if(openedImage  == null ){ //this is first card
+          openedImage =  images[i];
+          openedCard = cards[i];
+        } else { //second click on another image
+          if(openedImage == images[i] ){ // matched images
+            // score changed
+            //add class mached
+            cards[i].classList.add('matched');
+            openedCard.classList.add('matched');
+
+          } else{
+            let tempFirstCard = openedCard;
+            setTimeout(function(){ //reset cards
+              cards[i].innerHTML = '<div class="back-card"><img src="assets/images/memory-logo.png"></div>'; //reset current box
+              tempFirstCard.innerHTML = '<div class="back-card"><img src="assets/images/memory-logo.png"></div>';
+              tempFirstCard.classList.remove('open','card-is-flipped');
+              cards[i].classList.remove('open','card-is-flipped');
+            },500)
+            var audio = new Audio('assets/audio/NFF-wrong-move.wav');
+            audio.play();
+            failedNum ++ ;
+            if(failedNum == 3){
+              failedNum = 0;
+              if (scoreRateNum > 1 ){
+                scoreRateNum -= 1;
+                drawStars(scoreRateNum);
+              }
+            }
+            //add class notmatched
+          }
+          openedImage = null;
+        }
+
       }
 
-    }
-
-  }); //EOF  on Click
+    }); //EOF  on Click
+  }
 }
-
-
+startGame();
 // function reset(){
 //   openedImage = null;
 //   openedCard = null;
